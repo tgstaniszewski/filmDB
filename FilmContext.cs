973f6c1fc621e2e24f30ff1e1d83d51d7389061f
Tomasz Staniszewski <s9355ts@ms.wwsi.edu.pl>
2021-10-23 16:49:12 +0200

    Connection with database

---
+++ FilmContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmDB.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmDB
+{
+    public class FilmContext: DbContext
+    {
+        DbSet<FilmModel> Films { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string con = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FilmDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            optionsBuilder.UseSqlServer(con);
+        }
+    }
+}
